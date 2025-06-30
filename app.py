86e58da1df7d50085e18cf1b423a105cc0e3f108
Manikanta <manikantamummidi5@gmail.com>
2025-06-30 15:11:51 +0530

    Apply Black formatting for consistency

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return "Hello, DevOps World!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -2,11 +2,12 @@
 
 from app import app
 
+
 def test_home():
     # Use Flask's test client to simulate requests
     client = app.test_client()
-    response = client.get('/')
-    
+    response = client.get("/")
+
     # Assert the response is OK
     assert response.status_code == 200
 
